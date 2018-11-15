8dd26f165180f93d6e4840aa14da4bf755d3dcb5
ushijima <ushinokomoriuta@gmail.com>
2018-11-16 01:28:26 +0900

    ファイル移動関係修正

--- chat_theme.php
+++ chat_theme.php
@@ -65,11 +65,13 @@ if(!empty($_FILES)){
           // ファイル名についてのその他のバリデーションも、適切に行いましょう。
           // $name = basename($_FILES["file"]["name"][$key]);
           $name = $_FILES["file"]["name"][$key];
-          $_SESSION['upfile']['name'][$key] = $name;
-          $_SESSION['upfile']['error'][$key] = $_FILES["file"]["error"][$key];
+          // $_SESSION['upfile']['name'][$key] = $name;
+          // $_SESSION['upfile']['error'][$key] = $_FILES["file"]["error"][$key];
+          $_SESSION['upfile'] = $_FILES['file'];
           // $file_list[] = preg_split("/[.]/",$_SESSION['upfile'][$key]);
           // $_SESSION['filelist'] = $file_list;
                   //imgのディレクトリを書き込みが出来るようにその他のユーザーにも書き込み権限を与える
+        system('sudo chmod 0777 '.$_SERVER['DOCUMENT_ROOT'].'view');
         system('sudo chmod 0777 '.$_SERVER['DOCUMENT_ROOT'].'view/img');
         //ディレクトリが作成されていない場合、新規で作成する
         if(!file_exists(THEME_IMAGE)){
@@ -83,7 +85,10 @@ if(!empty($_FILES)){
           chmod(THEME_IMAGE_TMP,0777);
         }
         system('sudo chmod 0777 '.THEME_IMAGE_TMP);
-          move_uploaded_file($tmp_name, THEME_IMAGE_TMP);
+          move_uploaded_file($name, THEME_IMAGE_TMP.'/'.$name);
+        //書き込み後は、権限を元に戻す
+        system('sudo chmod 0775 '.$_SERVER['DOCUMENT_ROOT'].'view');
+        system('sudo chmod 0775 '.$_SERVER['DOCUMENT_ROOT'].'view/img');
       }
   }
 }
@@ -128,7 +133,7 @@ if(!empty($_POST['chat_theme_create'])){
   if($_GET['edit_type'] == 'register'){
     //エラーがなければ登録処理を行う
     if(empty($_POST['err'])){
-      $chat->setGroupChat($_POST['GROUP_GHAT_NO'],$_POST['GROUP_CHAT_TITLE'],$_POST['GROUP_CHAT_MEMO'],$_SESSION['file'],$_POST['modifiable'],$_SESSION['belong_member']);
+      $chat->setGroupChat($_POST['GROUP_GHAT_NO'],$_POST['GROUP_CHAT_TITLE'],$_POST['GROUP_CHAT_MEMO'],$_SESSION['upfile'],$_POST['modifiable'],$_SESSION['belong_member']);
       //チャットの一覧ページ(ダイレクトページとテーマチャットが一覧で見れるページ)メッセージは、
       $_SESSION['edit_topic_msg'] = 'トピックが'.$chat->getGroupChatEditType($_GET['edit_type']).'されました';
       //登録がうまく行った場合は、ページ遷移させずに表示させていたセレクトボックスの値などのデータをセッションに入れていたものをクリアする
